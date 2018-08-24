a81d59a41dc47b780111e648d7481790d3a70fe7
AndresMRM <andres@inventati.org>
2018-08-24 07:32:32 -0300

    configura documentação swagger

--- agorapi/agorapi/urls.py
+++ agorapi/agorapi/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from rest_framework_swagger.views import get_swagger_view
 
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include('api.urls')),
+    url(r'^$', schema_view)
 ]
