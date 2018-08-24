0743a0ca3085c4e93e1476a544acdbcae23a3253
AndresMRM <andres@inventati.org>
2018-08-24 10:40:46 -0300

    refac

--- agorapi/api/utils.py
+++ agorapi/api/utils.py
@@ -1,11 +1,11 @@
-import glob
+from glob import glob
 import pandas as pd
 
 prop_data_files = '../../agora-digital/data/*/*prop*.csv'
 
 
 def get_props(nonans=True):
-    df = pd.concat([pd.read_csv(f) for f in glob.glob(prop_data_files)], sort=False)
+    df = pd.concat([pd.read_csv(f) for f in glob(prop_data_files)], sort=False)
     if nonans:
         df = df.where((pd.notnull(df)), None)
     return df
