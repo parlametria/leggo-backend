c44a5310f8e3da8d670ffeeaee7293201346ddb8
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2018-09-27 09:43:25 -0300

    Retirada url desnecessária

--- api/urls.py
+++ api/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     url(r'^info/?$', views.Info.as_view()),
     url(r'^proposicoes/(?P<casa>[a-z]+)/(?P<id_ext>[0-9]+)/?$',
         views.ProposicaoDetail.as_view()),
-    url(r'^proposicoes/?$', views.ProposicaoList.as_view()),
-    url(r'^proposicoes/(?P<id_ext>[0-9]+)/fases/?$', views.Info.as_view())
+    url(r'^proposicoes/?$', views.ProposicaoList.as_view())
 ]
