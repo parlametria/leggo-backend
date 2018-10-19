a8eaabb182e3f78bb1259b563859a982070211e7
Jair Guedes <jairguedesferreira@gmail.com>
2018-10-19 13:52:12 -0300

    Coloca a data referencia como parametro

--- api/views.py
+++ api/views.py
@@ -147,7 +147,19 @@ class ProgressoList(generics.ListAPIView):
         queryset = Progresso.objects.filter(
             etapa__casa=casa, etapa__id_ext=id_ext)
 
-        queryset = queryset.filter()
+        try:
+            hoje = datetime.today() if data_referencia is None else datetime.strptime(	
+                data_referencia, '%Y-%m-%d')	
+        except ValueError:	
+            print(	
+                f'Data de referência ({data_referencia}) inválida. '	
+                'Utilizando data atual como data de referência.')	
+            hoje = datetime.today()	
+        
+        if(data_referencia == None):
+            queryset = queryset.filter()
+        else:
+            queryset = queryset.filter(data_inicio__lte=hoje)
 
         return queryset
 
