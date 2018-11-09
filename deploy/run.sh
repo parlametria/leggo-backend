29af341febf9153cc239b890666ba332f394006b
AndresMRM <andres@inventati.org>
2018-11-09 17:05:04 -0200

    faz deploy deletar BD e reimportá-lo

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 cp /agora-digital-backend/deploy/nginx/app.conf /nginx_config/ \
+    && ./manage.py flush --no-input \
     && ./manage.py migrate \
+    && ./manage.py import_data \
     && ./manage.py collectstatic --noinput \
     && uwsgi --ini /agora-digital-backend/deploy/uwsgi.ini
