eefd772688e2d3e1fa94c050afefdaa3713720dd
Jair Guedes <jairguedesferreira@gmail.com>
2019-04-24 11:39:49 -0300

    Adiciona o campo hora a pauta

--- api/models.py
+++ api/models.py
@@ -325,6 +325,8 @@ class PautaHistorico(models.Model):
 
     data = models.DateField('data')
 
+    hora = models.TextField('hora')
+
     semana = models.IntegerField('semana')
 
     local = models.TextField(blank=True)
