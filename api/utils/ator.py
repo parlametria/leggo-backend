6885c6787c880a4a42b10ad422c03a21abf33107
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-08-09 16:39:53 -0300

    Padroniza string com nome do ator retornado pelo back para que partido e estado fiquem sempre na mesma posição.

--- api/utils/ator.py
+++ api/utils/ator.py
@@ -1,9 +1,8 @@
 def get_nome_partido_uf(nome, partido, uf):
 
     if(uf == 'nan'):
-        uf = ''
+        uf = '-'
 
     if(partido == 'nan'):
-        partido = ''
-    barra = '' if (partido == '' or uf == '') else '/'
-    return (nome + ' - ' + partido + barra + uf)
+        partido = '-'
+    return (nome + ' (' + partido + '/' + uf + ')')
