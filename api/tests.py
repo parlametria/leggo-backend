ae1cd03b3d5ecf9bbc326be43d05c19240134493
Juan Barros <juanlyrabarros@gmail.com>
2019-08-19 09:52:14 -0300

    Adiciona indice ao dado a ser carregado

--- api/tests.py
+++ api/tests.py
@@ -34,9 +34,9 @@ class ProposicaoTests(APITestCase):
         url_detail = (self.url + str(self.etapa_proposicao.id))
         response = self.client.get(url_detail)
 
-        self.assertEqual(response.data['data_apresentacao'],
+        self.assertEqual(response.data[0]['data_apresentacao'],
                          self.etapa_proposicao.data_apresentacao)
-        self.assertEqual(response.data['casa'], self.etapa_proposicao.casa)
+        self.assertEqual(response.data[0]['casa'], self.etapa_proposicao.casa)
 
 
 # class TemperaturaHistoricoTest(APITestCase):
