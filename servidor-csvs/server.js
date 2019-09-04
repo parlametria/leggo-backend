3d98c37eb13df6c6a139fb5671d4c490db2e2923
Jair Guedes <jairguedesferreira@gmail.com>
2019-09-04 11:28:42 -0300

    Adiciona o servidor-csvs

--- servidor-csvs/server.js
+++ servidor-csvs/server.js
@@ -5,7 +5,7 @@ const serveIndex = require('serve-index');
 const app = express();
 const PORT = process.env.PORT = 8080;
 
-app.use('/csvs', express.static('../data'), serveIndex('../data', {'icons': true}))
+app.use('/csvs', express.static('./agora-digital/exported'), serveIndex('./agora-digital/exported', {'icons': true}))
 
 app.listen(PORT, () => {
   console.log('Server is running at:',PORT);
