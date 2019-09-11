44a6623c1362cc0451b267cbf536abbdf0d7ba96
Jair Guedes <jairguedesferreira@gmail.com>
2019-09-11 11:15:05 -0300

    Adc ultima_temperatura ao serializer etapa_proposicao

--- api/views/views.py
+++ api/views/views.py
@@ -93,7 +93,7 @@ class EtapasDetailSerializer(serializers.ModelSerializer):
             'temperatura_historico', 'autores', 'relator_nome', 'casa_origem',
             'em_pauta', 'apelido', 'tema', 'status', 'resumo_tramitacao', 'top_atores',
             'top_important_atores', 'comissoes_passadas', 'temperatura_coeficiente',
-            'pauta_historico', 'temas', 'ultima_pressao',)
+            'pauta_historico', 'temas', 'ultima_pressao', 'ultima_temperatura')
 
 
 class ProposicaoDetailSerializer(serializers.ModelSerializer):
