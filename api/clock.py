00223ce76a7b4d09c43b8df68ea528836972c9f5
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-09-12 17:19:30 -0300

    Altera o horário do script de atualização.

--- api/clock.py
+++ api/clock.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 sched = BackgroundScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17, minute=17)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17, minute=22)
 def scheduled_job():
     print('This job is run every weekday at 5:20pm.')
 
