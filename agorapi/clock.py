dd8b6a497fa6a45a0a9dae9cea79cbd929229241
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-09-12 15:22:33 -0300

    Altera horário do schedule.

--- agorapi/clock.py
+++ agorapi/clock.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 sched = BackgroundScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=15, minute=20)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=15, minute=30)
 def scheduled_job():
     print('This job is run every weekday at 3:20pm.')
 
