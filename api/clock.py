a95e4c7477d0125b6804da17bc99deb197affc4d
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-09-13 12:20:34 -0300

    Adiciona comandos de flush e importação de dados ao clock.

--- api/clock.py
+++ api/clock.py
@@ -1,20 +1,21 @@
-try:
-    from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
+from django.core import management
+from django.core.management.commands import flush, import_data_from_remote
 
-    sched = BlockingScheduler()
-    print('Iniciando scheduler...')
 
-    @sched.scheduled_job('interval', minutes=1)
-    def timed_job():
-        try:
-            print('This job is run every one minute.')
-        except Exception as e:
-            print(e)
+sched = BlockingScheduler()
+print('Iniciando scheduler...')
+
+@sched.scheduled_job('interval', minutes=2)
+def timed_job():
+    try:
+        management.call_command(flush.Command(), verbosity=3)
+        mangement.call_command(import_data_from_remote.Command(), verbosity=3)
+    except Exception as e:
+        print(e)
 
 #@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17, minute=22)
 #def scheduled_job():
 #    print('This job is run every weekday at 5:20pm.')
 
-    sched.start()
-except Exception as e:
-    print(e)
+sched.start()
