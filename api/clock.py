46c2eccb7754cf9ac4d7a61739697de9345926d2
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-09-13 12:02:08 -0300

    Tenta identificar erro do script de clock.

--- api/clock.py
+++ api/clock.py
@@ -1,17 +1,20 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+try:
+    from apscheduler.schedulers.background import BackgroundScheduler
 
-sched = BackgroundScheduler()
-print('Iniciando scheduler...')
+    sched = BackgroundScheduler()
+    print('Iniciando scheduler...')
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    try:
-        print('This job is run every one minute.')
-    except Exception as e:
-        print(e)
+    @sched.scheduled_job('interval', minutes=1)
+    def timed_job():
+        try:
+            print('This job is run every one minute.')
+        except Exception as e:
+            print(e)
 
 #@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17, minute=22)
 #def scheduled_job():
 #    print('This job is run every weekday at 5:20pm.')
 
-sched.start()
+    sched.start()
+except Exception as e:
+    print(e)
