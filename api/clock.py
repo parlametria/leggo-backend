ebcb6d30ede39b4d68d1e800c407717cf782a170
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-09-13 11:25:44 -0300

    Define novo job a cada 3 minutos para teste.

--- api/clock.py
+++ api/clock.py
@@ -2,8 +2,15 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 sched = BackgroundScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17, minute=22)
-def scheduled_job():
-    print('This job is run every weekday at 5:20pm.')
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    try:
+        print('This job is run every three minutes.')
+    except Exception as e:
+        print(e)
+
+#@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17, minute=22)
+#def scheduled_job():
+#    print('This job is run every weekday at 5:20pm.')
 
 sched.start()
