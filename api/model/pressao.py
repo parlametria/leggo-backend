03a803fdd74294b1910a0bbab16899f292c3f7ec
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-03-26 15:55:46 -0300

    Inicia modificação em model de pressão

--- api/model/pressao.py
+++ api/model/pressao.py
@@ -1,5 +1,6 @@
 from django.db import models
 from api.model.proposicao import Proposicao
+from api.model.etapa_proposicao import Choices
 
 
 class Pressao(models.Model):
@@ -9,16 +10,31 @@ class Pressao(models.Model):
 
     id_leggo = models.IntegerField('Id da proposição principal no leggo.')
 
+    id_ext = models.IntegerField('ID Externo', default=-1, 
+        help_text='Id externo do sistema da casa.')
+
+    casas = Choices('camara senado')
+    casa = models.CharField(
+        max_length=6, choices=casas.items(), default='', 
+        help_text='Casa.')
+
     date = models.DateField('Dia da popularidade')
 
-    max_pressao_principal = models.FloatField(
-        'Pressão do nome formal e do apelido')
+    trends_max_pressao_principal = models.FloatField(
+        'Pressão do nome formal e do apelido', default=0)
 
-    max_pressao_rel = models.FloatField(
-        'Pressão dos termos relacionados')
+    trends_max_pressao_rel = models.FloatField(
+        'Pressão dos termos relacionados', default=0)
 
-    maximo_geral = models.FloatField(
-        'Pressão dos termos relacionados')
+    trends_max_popularity = models.FloatField(
+        'Pressão máxima entre os termos', default=0)
+
+    twitter_mean_popularity = models.FloatField(
+        'Pressão no twitter', default=0)
+
+    popularity = models.FloatField(
+        'Pressão combinada do Twitter e Google Trends', 
+        default=0)
 
     proposicao = models.ForeignKey(
         Proposicao, on_delete=models.CASCADE, related_name='pressao')
