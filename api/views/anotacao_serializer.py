a3ae51ef83c18cb1f4386c00501e5c41de4ea84b
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-05-25 10:14:23 -0300

    Adiciona parâmetro de data de início e de fim para filtrar anotações

--- api/views/anotacao_serializer.py
+++ api/views/anotacao_serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers, generics
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from api.model.anotacao import Anotacao
+from datetime import datetime
 
 
 class AnotacaoSerializer(serializers.ModelSerializer):
@@ -77,6 +78,14 @@ class AnotacaoList(generics.ListAPIView):
             openapi.Parameter(
                 'interesse', openapi.IN_PATH, 'Nome do interesse-alvo', type=openapi.TYPE_STRING),
             openapi.Parameter(
+                'data_inicio', openapi.IN_PATH,
+                'data de início do período de tempo ao qual as anotações devem ser modificadas',
+                type=openapi.TYPE_STRING),
+            openapi.Parameter(
+                'data_fim', openapi.IN_PATH,
+                'data de fim do período de tempo ao qual as anotações devem ser modificadas',
+                type=openapi.TYPE_STRING),
+            openapi.Parameter(
                 'peso', openapi.IN_PATH, 'Se deve retornar apenas as anotações tão ou mais importantes do que este valor', type=openapi.TYPE_INTEGER),
             openapi.Parameter(
                 'ultimas_n', openapi.IN_PATH, 'Número máximo de retorno das últimas anotações',
@@ -90,13 +99,40 @@ class AnotacaoList(generics.ListAPIView):
         '''
 
         interesseArg = self.request.query_params.get('interesse')
+        data_inicio = self.request.query_params.get('data_inicio', None)
+        data_fim = self.request.query_params.get('data_fim', None)
         peso = self.request.query_params.get('peso', 100)
         ultimos_n = self.request.query_params.get('ultimas_n', 10)
+
+        data_inicio_dt = None
+        data_fim_dt = None
 
         if not interesseArg:
             interesseArg = 'leggo'
 
         queryset = Anotacao.objects.filter(interesse=interesseArg)
+
+        try:
+            if data_inicio is not None:
+                data_inicio_dt = datetime.strptime(data_inicio, '%Y-%m-%d')
+        except ValueError:
+            print(f'Data de início ({data_inicio}) inválida. ')
+            data_inicio_dt = None
+
+        try:
+            data_fim_dt = (
+                datetime.today() if data_fim is None else datetime.strptime(
+                    data_fim, '%Y-%m-%d'))
+        except ValueError:
+            print(
+                f'Data de fim ({data_fim}) inválida. '
+                'Utilizando data atual como data de fim.')
+            data_fim_dt = datetime.today()
+
+        if data_inicio_dt is not None:
+            queryset = queryset.filter(data_ultima_modificacao__gte=data_inicio_dt)
+
+        queryset = queryset.filter(data_ultima_modificacao__lte=data_fim_dt)
         
         if peso:
             queryset = queryset.order_by('peso', '-data_ultima_modificacao').filter(peso__lte=peso)
