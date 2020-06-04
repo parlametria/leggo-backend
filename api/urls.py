6afbde1a2784ab7b3992291fc9ebb05c8c4fd33d
Luiz Augusto Morais <luizaugustomm@gmail.com>
2020-06-04 19:12:28 -0300

    Cria rota para nova view de parlamentares

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,8 @@ from api.views.progresso_serializer import ProgressoList
 from api.views.comissao_serializer import ComissaoList
 from api.views.pauta_serializer import PautaList
 from api.views.emenda_serializer import EmendasList
-from api.views.ator_serializer import AtoresList
+from api.views.ator_serializer import AtoresProposicaoList
+from api.views.parlamentar_serializer import ParlamentaresList
 from api.views.pressao_serializer import PressaoList
 from api.views.coautoria_node_serializer import CoautoriaNodeList
 from api.views.coautoria_edge_serializer import CoautoriaEdgeList
@@ -40,8 +41,10 @@ urlpatterns = [
         PautaList.as_view()),
     url(r'^emenda/(?P<casa>[a-z]+)/(?P<id_ext>[0-9]+)/?$',
         EmendasList.as_view()),
+    url(r'^parlamentares/?$',
+        ParlamentaresList.as_view()),
     url(r'^atores/(?P<id_leggo>[0-9]+)/?$',
-        AtoresList.as_view()),
+        AtoresProposicaoList.as_view()),
     url(r'^pressao/(?P<id_leggo>[0-9]+)/?$',
         PressaoList.as_view()),
     url(r'^coautorias_node/(?P<id>[0-9]+)/?$',
