d8e7dc1798c8734b52a799c065a9bdd086cd951e
Luiz Augusto Morais <luizaugustomm@gmail.com>
2020-06-05 11:30:57 -0300

    Adiciona quantidade de autorias de um parlamentar

--- api/views/parlamentar_serializer.py
+++ api/views/parlamentar_serializer.py
@@ -1,8 +1,8 @@
 
 from rest_framework import serializers, generics
 from api.model.ator import Atores
-from api.model.comissao import Comissao
-from django.db.models import Sum, OuterRef, Subquery
+from api.model.autoria import Autoria
+from django.db.models import Sum, Count, OuterRef, Subquery, Case, When, IntegerField
 
 
 # class DocumentosSerializer(serializers.Serializer):
@@ -20,6 +20,7 @@ class ParlamentaresSerializer(serializers.Serializer):
     bancada = serializers.CharField()
     total_documentos = serializers.IntegerField()
     peso_documentos = serializers.IntegerField()
+    n_autorias = serializers.IntegerField()
 
 class ParlamentaresList(generics.ListAPIView):
     '''
@@ -28,13 +29,16 @@ class ParlamentaresList(generics.ListAPIView):
     serializer_class = ParlamentaresSerializer
 
     def get_queryset(self):
-        # Reduz tabela para apenas um registro para cada parlamentar
-        parlamentares_unicos = Atores.objects.filter(id_autor=OuterRef('id_autor')).distinct('id_autor')
+        unicos = Atores.objects.filter(id_autor=OuterRef('id_autor')).distinct('id_autor')
+        autorias = Autoria.objects.filter(id_autor=OuterRef('id_autor')).values('id_autor').annotate(n_autorias=Count('id_autor'))
 
-        return Atores.objects.values('id_autor').annotate(total_documentos=Sum('num_documentos'),
-                                                          peso_documentos=Sum('peso_total_documentos'),
-                                                          nome=Subquery(parlamentares_unicos.values('nome_autor')),
-                                                          partido=Subquery(parlamentares_unicos.values('partido')),
-                                                          uf=Subquery(parlamentares_unicos.values('uf')),
-                                                          casa=Subquery(parlamentares_unicos.values('casa')),
-                                                          bancada=Subquery(parlamentares_unicos.values('bancada')))
+        return Atores.objects.values('id_autor').annotate(
+            total_documentos=Sum('num_documentos'),
+            peso_documentos=Sum('peso_total_documentos'),
+            nome=Subquery(unicos.values('nome_autor')[:1]),
+            partido=Subquery(unicos.values('partido')[:1]),
+            uf=Subquery(unicos.values('uf')[:1]),
+            casa=Subquery(unicos.values('casa')[:1]),
+            bancada=Subquery(unicos.values('bancada')[:1]),
+            n_autorias=Subquery(autorias.values('n_autorias'))
+        )
