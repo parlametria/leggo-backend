89b6fdbe17471e3dd31c12cc92156823e846f084
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-06-11 15:42:49 -0300

    Adiciona endpoint que retorna o peso político de um parlamentar específico

--- api/utils/peso_politico.py
+++ api/utils/peso_politico.py
@@ -1,5 +1,5 @@
 import json
-
+import requests
 
 from api.utils.csv_servers import post_req
 
@@ -9,14 +9,27 @@ URL_PESO_POLITICO = "https://perfil.parlametria.org/api/perfil/"
 
 def get_peso_politico_lista(lista_ids):
     try:
-      payload = {"parlamentares": lista_ids}
+        payload = {"parlamentares": lista_ids}
 
-      r = post_req(URL_PESO_POLITICO + "lista", payload)
-      processed_data = [
-         dict(id_autor=e["idParlamentarVoz"], peso_politico=e["pesoPolitico"])
-         for e in json.loads(r)
-      ]
-      return processed_data
+        r = post_req(URL_PESO_POLITICO + "lista", payload)
+        return json.loads(r)
 
-    except Exception as e:
-      return []
+    except Exception:
+        return []
+
+
+def get_peso_politico_parlamentar(id):
+    try:
+        r = requests.get(url=URL_PESO_POLITICO + str(id))
+        data = json.loads(r.text)
+
+        obj_arr = []
+
+        data["idParlamentarVoz"] = int(data["idParlamentarVoz"])
+
+        obj_arr.append(data)
+
+        return obj_arr
+
+    except Exception:
+        return []
--- api/views/peso_politico_serializer.py
+++ api/views/peso_politico_serializer.py
@@ -4,12 +4,15 @@ from drf_yasg.utils import swagger_auto_schema
 
 from api.model.ator import Atores
 from api.utils.filters import get_filtered_interesses
-from api.utils.peso_politico import get_peso_politico_lista
+from api.utils.peso_politico import (
+    get_peso_politico_lista,
+    get_peso_politico_parlamentar,
+)
 
 
 class PesoPoliticoSerializer(serializers.Serializer):
-    id_autor = serializers.IntegerField()
-    peso_politico = serializers.FloatField()
+    idParlamentarVoz = serializers.IntegerField()
+    pesoPolitico = serializers.FloatField()
 
 
 class PesoPoliticoLista(generics.ListAPIView):
@@ -19,6 +22,16 @@ class PesoPoliticoLista(generics.ListAPIView):
 
     serializer_class = PesoPoliticoSerializer
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                "interesse",
+                openapi.IN_PATH,
+                "Interesse que se deseja obter a lista com os parlamentares e seus pesos políticos",
+                type=openapi.TYPE_INTEGER,
+            )
+        ]
+    )
     def get_queryset(self):
         """
         Retorna dados de peso político para os atores de um determinado interesse.
@@ -35,6 +48,47 @@ class PesoPoliticoLista(generics.ListAPIView):
             )
         )
 
-        data = get_peso_politico_lista([1, 23396, 173531])
+        # TODO: Remover a linha abaixo quando o mapeamento estiver concluído
+        # data = get_peso_politico_lista([1, 23396, 173531])
+
+        data = get_peso_politico_lista(lista_ids)
+
+        return data
+
+
+class PesoPoliticoParlamentar(generics.ListAPIView):
+    """
+    Informação sobre o peso político dos parlamentares.
+    """
+
+    serializer_class = PesoPoliticoSerializer
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                "id",
+                openapi.IN_PATH,
+                "Id do autor que se deseja saber o peso político",
+                type=openapi.TYPE_INTEGER,
+            )
+        ]
+    )
+    def get_queryset(self):
+        """
+        Retorna dados de peso político para os atores de um determinado interesse.
+        Caso nenhum seja passado, o valor default será leggo.
+        """
+        id_autor_arg = self.kwargs["id"]
+
+        id = (
+            Atores.objects.filter(id_autor=id_autor_arg)
+            .values_list("id_autor", flat=True)
+            .first()
+        )
+
+        # TODO: Remover a linha abaixo quando o mapeamento estiver concluído
+        # id = 1204467
+
+        data = get_peso_politico_parlamentar(id)
 
         return data
