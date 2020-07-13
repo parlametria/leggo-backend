6bf544a1cac47d4e77fd6ea340342505db3b8e73
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2020-07-13 14:41:36 -0300

    consulta para comissoes que ja tiveram tramitacoes na agenda

--- api/utils/presidencia_comissao.py
+++ api/utils/presidencia_comissao.py
@@ -4,8 +4,8 @@ import requests
 URL_PRESIDENCIA_COMISSAO = "https://perfil.parlametria.org/api/busca-parlamentar"
 
 
-def get_comissao_parlamentar():
-
+def get_comissao_parlamentar(listaComissoesPassadas):
+    
     try:
         r = requests.get(url=URL_PRESIDENCIA_COMISSAO)
         data = json.loads(r.text)
@@ -14,25 +14,25 @@ def get_comissao_parlamentar():
 
         for obj in data:
             for index in obj['parlamentarComissoes']:
-                if(index['cargo'] == "Presidente"):
+                if(index['cargo'] == "Presidente" and index['infoComissao']['sigla'] in listaComissoesPassadas):
                     countComissoes += 1
                     obj_arr.append({
                         'id_comissao': index['idComissaoVoz'],
                         'id_autor': obj['idParlamentar'],
                         'id_autor_voz': obj['idParlamentarVoz'],
-                        'info_comissao': index['infoComissao'],
+                        'info_comissao': index['infoComissao']['sigla'],
                         'quantidade_comissao_presidente': countComissoes
                     })
                     countComissoes = 0
+
         return obj_arr
 
     except Exception as e:
         print(e)
         return []
 
-
-def get_comissao_parlamentar_id(id_parlamentar):
-    
+def get_comissao_parlamentar_id(id_parlamentar, listaComissoesPassadas):
+  
     try:
         r = requests.get(url=URL_PRESIDENCIA_COMISSAO)
         data = json.loads(r.text)
@@ -42,7 +42,7 @@ def get_comissao_parlamentar_id(id_parlamentar):
         for obj in data:
             if(obj['idParlamentarVoz'] == id_parlamentar):
                 for index in obj['parlamentarComissoes']:
-                    if(index['cargo'] == "Presidente"):
+                    if(index['cargo'] == "Presidente" and index['infoComissao']['sigla'] in listaComissoesPassadas):
                         countComissoes += 1
                         obj_arr.append({
                             'id_comissao': index['idComissaoVoz'],
@@ -52,9 +52,9 @@ def get_comissao_parlamentar_id(id_parlamentar):
                             'quantidade_comissao_presidente': countComissoes
                         })
                         countComissoes = 0
+
         return obj_arr
 
     except Exception as e:
         print(e)
         return []
-   
--- api/views/presidencia_comissao_serializer.py
+++ api/views/presidencia_comissao_serializer.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers, generics
+from api.model.proposicao import Proposicao
+from api.views.interesse_serializer import InteresseSerializer
+from api.model.tramitacao_event import TramitacaoEvent
+from api.model.etapa_proposicao import EtapaProposicao
+
+from api.utils.filters import get_time_filtered_pauta, get_filtered_interesses
 from api.utils.presidencia_comissao import (
     get_comissao_parlamentar,
     get_comissao_parlamentar_id
-)
-
+    )
 
 class PresidenciaComissaoSerializer(serializers.Serializer):
     id_autor = serializers.IntegerField()
@@ -11,6 +16,8 @@ class PresidenciaComissaoSerializer(serializers.Serializer):
     id_autor_voz = serializers.IntegerField()
     quantidade_comissao_presidente = serializers.IntegerField()
     info_comissao = serializers.CharField()
+    
+    interesse = InteresseSerializer(many=True, read_only=True)
 
 
 class PresidenciaComissaoLista(generics.ListAPIView):
@@ -18,11 +25,33 @@ class PresidenciaComissaoLista(generics.ListAPIView):
     serializer_class = PresidenciaComissaoSerializer
 
     def get_queryset(self):
+        
         '''
-        Retorna informações sobre os parlamentares presidentes de comissões.
+        Retorna informações sobre os parlamentares presidentes de comissões que 
+        passaram por tramitação.
         '''
 
-        data = get_comissao_parlamentar()
+        interesseArg = self.request.query_params.get("interesse")
+
+        if interesseArg is None:
+            interesseArg = "leggo"
+
+        consultaTramitacao = TramitacaoEvent.objects.raw("SELECT distinct(sigla_local) as id FROM api_tramitacaoevent "
+
+        + "LEFT JOIN api_etapaproposicao as etapa ON etapa_proposicao_id = etapa.id "
+
+        + "LEFT JOIN api_proposicao as prop ON proposicao_id = prop.id "
+
+        + "LEFT JOIN api_interesse as interesse ON prop.id = interesse.id "
+
+        + "WHERE interesse.interesse = %s ", [interesseArg])
+
+        listaComissoesPassadas = []
+
+        for obj in consultaTramitacao:
+            listaComissoesPassadas.append(obj.id)
+
+        data = get_comissao_parlamentar(listaComissoesPassadas)
 
         return data
 
@@ -31,12 +60,32 @@ class PresidenciaComissaoParlamentar(generics.ListAPIView):
     serializer_class = PresidenciaComissaoSerializer
 
     def get_queryset(self):
-    
+
         '''
         Retorna informações sobre os parlamentares presidentes de comissões de 
         acordo com o id.
         '''
-        
+
+        interesseArg = self.request.query_params.get("interesse")
+
+        if interesseArg is None:
+            interesseArg = "leggo"
+
+        consultaTramitacao = TramitacaoEvent.objects.raw("SELECT distinct(sigla_local) as id FROM api_tramitacaoevent "
+
+        + "LEFT JOIN api_etapaproposicao as etapa ON etapa_proposicao_id = etapa.id "
+
+        + "LEFT JOIN api_proposicao as prop ON proposicao_id = prop.id "
+
+        + "LEFT JOIN api_interesse as interesse ON prop.id = interesse.id "
+
+        + "WHERE interesse.interesse = %s ", [interesseArg])
+
+        listaComissoesPassadas = []
+
+        for obj in consultaTramitacao:
+            listaComissoesPassadas.append(obj.id)
+
         id_autor_arg = self.kwargs["id"]
 
-        return get_comissao_parlamentar_id(id_autor_arg)
+        return get_comissao_parlamentar_id(id_autor_arg, listaComissoesPassadas) 
