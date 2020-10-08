bd69ca7f650661aa8f49350030265f48bbb6174f
Joao Menezes <joao.menezes@ccc.ufcg.edu.br>
2020-10-08 11:49:31 -0300

    fix lint

--- api/utils/sigla.py
+++ api/utils/sigla.py
@@ -1,7 +1,7 @@
 def cria_sigla(etapa):
-  sigla = str(
-    etapa.sigla_tipo + ' ' +
-    str(etapa.numero) + '/' +
-    str(etapa.data_apresentacao.year)
-  )
-  return sigla
+    sigla = str(
+        etapa.sigla_tipo + ' ' +
+        str(etapa.numero) + '/' +
+        str(etapa.data_apresentacao.year)
+    )
+    return sigla
