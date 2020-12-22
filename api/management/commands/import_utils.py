6d42094a9f755fa0cedfd610e6854a2e05e92354
Joao Menezes <joao.menezes@ccc.ufcg.edu.br>
2020-12-22 08:59:44 -0300

    remove print

--- api/management/commands/import_utils.py
+++ api/management/commands/import_utils.py
@@ -733,7 +733,7 @@ def import_destaques():
 
     for group_index in grouped.groups:
         id_leggo = {"id_leggo": group_index}
-        print(id_leggo)
+
         prop = get_proposicao(id_leggo, "Destaques")
         group_df = (
                 grouped.get_group(group_index)
