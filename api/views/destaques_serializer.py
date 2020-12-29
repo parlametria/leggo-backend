52e39aa07bcbdbf4e0fc1b557c23d4d9016348e0
gileadekelvin <gileadekelvin@gmail.com>
2020-12-29 15:16:23 -0300

    Adiciona colunas da casa de origem e revisora

--- api/views/destaques_serializer.py
+++ api/views/destaques_serializer.py
@@ -7,7 +7,8 @@ class DestaquesDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Destaques
         fields = (
-            "id_leggo", "criterio_aprovada_em_uma_casa", "casa_aprovacao",
+            "id_leggo", "casa_origem", "casa_revisora",
+            "criterio_aprovada_em_uma_casa", "casa_aprovacao",
             "data_aprovacao", "criterio_avancou_comissoes",
             "comissoes_camara", "comissoes_senado",
             "criterio_req_urgencia_apresentado", "casa_req_urgencia_apresentado",
