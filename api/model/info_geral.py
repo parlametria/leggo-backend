35e311b89a18b66c154bf776ea3643c907000b21
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2021-01-28 10:01:10 -0300

    Desfaz upgrade

--- api/model/info_geral.py
+++ api/model/info_geral.py
@@ -1,7 +1,8 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 
 class InfoGerais(models.Model):
 
     name = models.TextField()
-    value = models.JSONField()
+    value = JSONField()
