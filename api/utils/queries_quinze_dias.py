78d36689efa920f833951c0161865cec1fa6cfa0
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2021-04-09 15:18:34 -0300

    adicionando ultima pressao em quinze dias

---
+++ api/utils/queries_quinze_dias.py
@@ -0,0 +1,27 @@
+def queryPressaoQuinzeDias(interesse):
+
+    p = f"SELECT DISTINCT ON (p.id_leggo) \
+            1 AS id, \
+            aux_top.id_leggo, \
+            trends_max_popularity, \
+            pressao_quinze_dias \
+            FROM  api_pressao p \
+            INNER JOIN \
+            (SELECT DISTINCT(id_leggo) \
+            FROM api_interesse \
+            WHERE interesse = '{interesse}') AS interesse_aux \
+            ON p.id_leggo = interesse_aux.id_leggo \
+            LEFT JOIN \
+            (WITH summary AS ( \
+                SELECT p.id_leggo,  \
+                    p.trends_max_popularity AS pressao_quinze_dias,  \
+                    ROW_NUMBER() OVER(PARTITION BY p.id_leggo  \
+                                            ORDER BY p.date DESC) AS rk \
+                FROM api_pressao p) \
+            SELECT s.* \
+            FROM summary s \
+            WHERE s.rk = 3) AS aux_top  ON \
+            aux_top.id_leggo = p.id_leggo \
+            ORDER  BY p.id_leggo, p.date DESC;"
+
+    return p
