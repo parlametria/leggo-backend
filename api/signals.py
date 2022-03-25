68d1716c88de39711e687e2ee06f58743d5a5979
andersonms1 <andersoonn.ams@gmail.com>
2022-03-24 23:46:10 -0300

    Remove bug de print dos usuarios da proposicao

--- api/signals.py
+++ api/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, pre_delete, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from api.model.proposicao import Proposicao
@@ -7,7 +7,7 @@ from usuario.models import UsuarioProposicao
 
 @receiver(post_save, sender=Proposicao, dispatch_uid="update_user_ref")
 def update_user(sender, instance, **kwargs):
-    print("singal")
+    print("Signal")
     encontrouProposicao = UsuarioProposicao.objects.filter(proposicao=instance.id_leggo)
     if not encontrouProposicao:
         print("Não encontrado!")
@@ -16,4 +16,5 @@ def update_user(sender, instance, **kwargs):
         up.save()
     else:
         print("Usuários interessados")
-        print(encontrouProposicao.usuarios)
+        print(encontrouProposicao)
+        print(encontrouProposicao[0].usuarios.all())
--- usuario/admin.py
+++ usuario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UsuarioProposicao
+from api.model.proposicao import Proposicao
+
+admin.site.register(UsuarioProposicao)
+admin.site.register(Proposicao)
 
 # Register your models here.
